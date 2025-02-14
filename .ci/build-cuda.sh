e7e91fa16bcfc284fe4125ae96c7949e429378db
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-02-14 16:12:09 +0100

    debug

--- .ci/build-cuda.sh
+++ .ci/build-cuda.sh
@@ -11,12 +11,13 @@ SPACK_INSTALL_TREE=/dev/shm/spack-install
 spack env create -d ./spack-env
 # add local repository with current sirius recipe
 spack -e ./spack-env repo add $REPO
-
 spack -e ./spack-env config add "packages:all:variants:[cuda_arch=${CUDA_ARCH},+cuda]"
+# debug
+cat ./spack-env/spack.yaml
+spack -e ./spack-env config add config:install_tree:$SPACK_INSTALL_TREE
 
 spack -e ./spack-env add $SPEC
 
-spack -e ./spack-env config add config:install_tree:$SPACK_INSTALL_TREE
 
 # build sirius from source
 spack -e ./spack-env develop -p $PWD q-e-sirius@=develop-ristretto ^sirius@develop+cuda
