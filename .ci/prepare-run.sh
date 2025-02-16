63fddae3d06f2ae5338ab4e34e8fd0c8a4e36e1c
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-02-16 21:36:51 +0100

    cleanup

--- .ci/prepare-run.sh
+++ .ci/prepare-run.sh
@@ -3,21 +3,15 @@
 # spack install tree from the artifact to /dev/shm
 # and sync all ranks
 
-module load python
-
 if [ $SLURM_LOCALID -eq 0 ]; then
     tar xf ./installdir.tar -C /
     touch /dev/shm/unpack_done_$CI_JOB_ID
-    module load uv
-    uv venv env
-    source ./env/bin/activate
-    uv pip install pyyaml
 fi
 # wait for tar unpack
 while [ ! -f /dev/shm/unpack_done_$CI_JOB_ID ]; do
     sleep 0.2
 done
 
-source ./env/bin/activate
-
+# load pip env with pyyaml
+source /user-environment/venv/bin/activate
 export PATH=/dev/shm/spack-install/q-e-sirius-develop-ristretto/bin/:$PATH
