48f33cc8f2ac7484227329d3a79147c07c2b3bb0
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-02-16 21:15:56 +0100

    uv env creation only once per node

--- .ci/prepare-run.sh
+++ .ci/prepare-run.sh
@@ -3,20 +3,21 @@
 # spack install tree from the artifact to /dev/shm
 # and sync all ranks
 
+module load python
 
 if [ $SLURM_LOCALID -eq 0 ]; then
     tar xf ./installdir.tar -C /
     touch /dev/shm/unpack_done_$CI_JOB_ID
+    module load uv
+    uv venv env
+    source ./env/bin/activate
+    uv pip install pyyaml
 fi
 # wait for tar unpack
 while [ ! -f /dev/shm/unpack_done_$CI_JOB_ID ]; do
     sleep 0.2
 done
 
-module load python uv
-
-uv venv env
 source ./env/bin/activate
-uv pip install pyyaml
 
 export PATH=/dev/shm/spack-install/q-e-sirius-develop-ristretto/bin/:$PATH
