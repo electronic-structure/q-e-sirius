c6f71c99c9bdd762047d93f24790184e8629dcf0
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-02-17 18:26:06 +0100

    avoid deadlock

--- .ci/prepare-run.sh
+++ .ci/prepare-run.sh
@@ -3,7 +3,8 @@
 # spack install tree from the artifact to /dev/shm
 # and sync all ranks
 
-if [ $SLURM_LOCALID -eq 0 ]; then
+# unpack if running within sbatch and also on baremetal (SLURM_LOCALID undef)
+if [ -z "$SLURM_LOCALID" ] || [ $SLURM_LOCALID -eq 0 ]; then
     tar xf ./installdir.tar -C /
     touch /dev/shm/unpack_done_$CI_JOB_ID
 fi
