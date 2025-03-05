71f0650a1c15f838759963b7986dd11553d012ea
Simon Pintarelli <simon.pintarelli@cscs.ch>
2025-03-05 16:40:35 +0100

    run spack in bwrap sandbox, hide home directory

--- .ci/build-cuda.sh
+++ .ci/build-cuda.sh
@@ -16,10 +16,8 @@ spack -e ./spack-env config add "packages:all:variants:[cuda_arch=${CUDA_ARCH},+
 cat ./spack-env/spack.yaml
 
 # workaround, first command fails asking to update config format, doesn't make any sense, cannot reproduce on cli
-#spack -e ./spack-env config add config:install_tree:$SPACK_INSTALL_TREE
-yq w -i ./spack-env/spack.yaml 'spack.config.install_tree.root' $SPACK_INSTALL_TREE
-#spack -e ./spack-env config add 'config:install_tree:projections:all:"{name}-{version}"'
-yq w -i ./spack-env/spack.yaml 'spack.config.install_tree.projections.all' '{name}-{version}'
+spack -e ./spack-env config add config:install_tree:$SPACK_INSTALL_TREE
+spack -e ./spack-env config add 'config:install_tree:projections:all:"{name}-{version}"'
 # debug
 cat ./spack-env/spack.yaml
 
