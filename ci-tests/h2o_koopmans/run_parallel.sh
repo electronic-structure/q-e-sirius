d077b09aebf218158728bb27692d5c8e4f12a1e7
Anton Kozhevnikov <toxa81@gmail.com>
2025-10-24 15:39:12 +0200

    fixes

--- ci-tests/h2o_koopmans/run_parallel.sh
+++ ci-tests/h2o_koopmans/run_parallel.sh
@@ -2,7 +2,12 @@
 
 set -ex
 
-srun -N1 -n4 -c2 pw.x -i h2o.scf.in -use_qe_scf
-srun -N1 -n4 -c2 kcw.x -i h2o.kcw-wann2kcw.in
-srun -N1 -n4 -c2 kcw.x -i h2o.kcw-screen.in
+srun -N1 -n4 -c2 ${QE_PATH}/pw.x -i h2o.scf.in -use_qe_scf
+srun -N1 -n4 -c2 ${QE_PATH}/kcw.x -i h2o.kcw-wann2kcw.in
+srun -N1 -n4 -c2 ${QE_PATH}/kcw.x -i h2o.kcw-screen.in
+
+# load python pyyaml
+source /user-environment/venv/bin/activate
+
+srun -n1 python3 ../kcw_diff.py kcw.ref.yml kcw.yml
 
