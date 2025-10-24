89ea32672168ac93ae99bf2b3a6eb4d75187b78a
Anton Kozhevnikov <toxa81@gmail.com>
2025-10-24 16:07:04 +0200

    cleanup

--- ci-tests/h2o_koopmans/run_parallel.sh
+++ ci-tests/h2o_koopmans/run_parallel.sh
@@ -2,12 +2,11 @@
 
 set -ex
 
-srun -N1 -n4 -c2 ${QE_PATH}/pw.x -i h2o.scf.in -use_qe_scf
-srun -N1 -n4 -c2 ${QE_PATH}/kcw.x -i h2o.kcw-wann2kcw.in
-srun -N1 -n4 -c2 ${QE_PATH}/kcw.x -i h2o.kcw-screen.in
+srun ${QE_PATH}/pw.x -i h2o.scf.in -use_qe_scf
+srun ${QE_PATH}/kcw.x -i h2o.kcw-wann2kcw.in
+srun ${QE_PATH}/kcw.x -i h2o.kcw-screen.in
 
 # load python pyyaml
 source /user-environment/venv/bin/activate
-
 srun -n1 python3 ../kcw_diff.py kcw.ref.yml kcw.yml
 
