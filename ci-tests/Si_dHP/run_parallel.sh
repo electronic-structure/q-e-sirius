999ab0514ae906f09280acad8065e93ec7453ca1
PSettembri <paolo@DESKTOP-V2D595E>
2026-06-30 12:01:17 +0200

    Fixed dHP ci-test and ref file formatting

--- ci-tests/Si_dHP/run_parallel.sh
+++ ci-tests/Si_dHP/run_parallel.sh
@@ -3,7 +3,7 @@
 set -ex
 
 #scf
-srun ${QE_PATH}/pw.x -npool 2 -i Si.scf.in -use_qe_scf
+srun ${QE_PATH}/pw.x -npool 4 -i Si.scf.in -use_qe_scf
 
 #wannier pp
 srun -n1 ${QE_PATH}/wannier90.x -pp Si
@@ -16,6 +16,6 @@ srun -n1 ${QE_PATH}/wannier90.x Si
 srun -n1 cat Si.wout
 
 #crpa
-srun ${QE_PATH}/crpa.x -i Si.crpa.in -npool 2
+srun ${QE_PATH}/crpa.x -i Si.crpa.in -npool 4
 source /user-environment/venv/bin/activate
 srun -n1    python3 ../crpa_diff.py crpa_iq1.ref.yml crpa_iq1.yml
